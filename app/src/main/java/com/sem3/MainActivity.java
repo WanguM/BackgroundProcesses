2e79d5b706349f0ff90740689f94347d317fbd5c
WanguM <susanwangu@gmail.com>
2018-11-22 16:09:52 +0300

    typography and layouts

--- app/src/main/java/com/sem3/MainActivity.java
+++ app/src/main/java/com/sem3/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +26,32 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(receiver, filter);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
+@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+
+        case R.id.Settings:
+            Toast.makeText(this,"Settings",Toast.LENGTH_LONG).show();
+            return true;
+
+        case R.id.About:
+            Toast.makeText(this,"About",Toast.LENGTH_LONG).show();
+
+        case R.id.Contact:
+            Toast.makeText(this,"Contact",Toast.LENGTH_LONG).show();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+    }
+
+    }
+
 }
